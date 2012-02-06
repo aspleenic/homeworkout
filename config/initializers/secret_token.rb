e89a3397c9ba3ee8d8682b1456a69644fc831c67
PJ Hagerty <pjhagerty@gmail.com>
2012-02-06 15:51:15 -0500

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homeworkout::Application.config.secret_token = '79f3b88ea3b5e896a47448fc5b7420825366dfe59703f15f18030073afc94c80271fdc85d497f984030adc16c86caef0612cea9ef9542e60e6c3584cbf438eb2'
